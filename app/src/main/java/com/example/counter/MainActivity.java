154743dd32702ab410f9a78b509ed4773aaeb343
Aisuluu817 <a.arstanbaeva01@gmail.com>
2020-07-04 10:57:58 +0600

    landscape mode onSaveInstanceState()

--- app/src/main/java/com/example/counter/MainActivity.java
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -1,32 +1,56 @@
 package com.example.counter;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    int counter;
+    TextView current;
+    Button count, reset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    int counter = 0;
-    TextView current ;
-    public void Count(View view) {
-       current = findViewById(R.id.current);
-        counter++;
-        current.setText(String.format("%04d", counter));
-
-    }
-    public void Reset(View view) {
+        count = (Button) findViewById(R.id.count);
+        reset = (Button) findViewById(R.id.reset);
         current = findViewById(R.id.current);
-        counter = 0;
+
+        if (savedInstanceState != null) {
+            counter = savedInstanceState.getInt("counter");
+        }
         current.setText(String.format("%04d", counter));
+
+        count.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter++;
+                current.setText(String.format("%04d", counter));
+            }
+        });
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter = 0;
+                current.setText(String.format("%04d", counter));
+            }
+        });
+
+    }
+
+    @Override
+    protected void onSaveInstanceState (@NonNull Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putInt("counter", counter);
+
     }
 }
